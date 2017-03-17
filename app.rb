8fb40632393db77425de2c62dad537655a0d2048
watsy0007 <watsy0007@gmail.com>
2017-03-17 19:56:06 +0800

    feat(users): 新增用户增删改查

--- app.rb
+++ app.rb
@@ -10,3 +10,31 @@ def json(data = {}, code = 200)
   status code
   data.to_json
 end
+
+users = data('users')
+
+get '/users' do
+  json users
+end
+
+get '/users/:id' do
+  user = users.find do |item|
+    item['id'] == params['id'].to_i
+  end
+  json user
+end
+
+post '/users' do
+  user = {}
+  user['id'] = users.empty? ? 1 : (users.last['id'] + 1)
+  user['name'] = params['name']
+  users << user
+  json user, 201
+end
+
+delete '/users/:id' do
+  users = users.delete_if do |item|
+    item['id'] == params['id'].to_i
+  end
+  json user.empty? ? {} : users.first
+end
